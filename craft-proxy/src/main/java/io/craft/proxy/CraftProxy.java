f644e61e66ca24931cef2010d023f7a56141a3b2
liangzhijie <liangzhijie@orkotech.com>
2019-02-27 16:42:48 +0800

    添加服务发现和proxy请求转发

--- craft-proxy/src/main/java/io/craft/proxy/CraftProxy.java
+++ craft-proxy/src/main/java/io/craft/proxy/CraftProxy.java
@@ -1,34 +1,117 @@
 package io.craft.proxy;
 
 
-import io.craft.core.config.EtcdClient;
-import io.craft.proxy.util.PropertyUtil;
+import io.craft.core.codec.CraftThrowableEncoder;
+import io.craft.core.constant.Constants;
+import io.craft.core.spring.PropertyManager;
+import io.craft.proxy.handler.ProxyMessageHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class CraftProxy {
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
-    private String host;
+@Slf4j
+public class CraftProxy implements Closeable {
+    private ConfigurableApplicationContext applicationContext;
+    private PropertyManager propertyManager;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+    private Channel channel;
 
-    private int port;
+    public void proxy() throws InterruptedException, IOException {
+        applicationContext = new ClassPathXmlApplicationContext("proxy.xml");
+        propertyManager = applicationContext.getBean(PropertyManager.class);
+        String host = propertyManager.getProperty(Constants.APPLICATION_HOST);
 
-    private String namespace;
+        int port = Integer.valueOf(propertyManager.getProperty(Constants.APPLICATION_PORT));
 
-    CraftProxy() {
+        CraftThrowableEncoder exceptionEncoder = new CraftThrowableEncoder();
 
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        bossGroup = new NioEventLoopGroup();
+
+        workerGroup = new NioEventLoopGroup();
+        try {
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(host, port))
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            logger.info("channel id = {}", ch.id());
+                            ch.pipeline().addLast(exceptionEncoder).addLast(new ProxyMessageHandler(applicationContext));
+                        }
+                    });
+            ChannelFuture f = bootstrap.bind().sync();
+            //端口监听成功
+            logger.debug("proxy start success on {}:{}", host, port);
+
+            channel = f.channel();
+
+            channel.closeFuture().sync();
+        } finally {
+            close();
+        }
+    }
+
+    @Override
+    public synchronized void close() throws IOException {
+        //等待1s, 避免有些还没感知到服务下线
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+        //关闭端口监听
+        try {
+            if (channel != null) {
+                channel.close().sync();
+                channel = null;
+                logger.debug("channel close successful");
+            }
+        } catch (InterruptedException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+        //关闭boss event loop
+        try {
+            if (bossGroup != null) {
+                bossGroup.shutdownGracefully().sync();
+                bossGroup = null;
+                logger.debug("boss group close successful");
+            }
+        } catch (InterruptedException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+        //关闭worker event loop
+        try {
+            if (workerGroup != null) {
+                workerGroup.shutdownGracefully().sync();
+                workerGroup = null;
+                logger.debug("worker group close successful");
+            }
+        } catch (InterruptedException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+        //关闭spring容器
+        if (applicationContext != null) {
+            applicationContext.close();
+            applicationContext = null;
+            logger.debug("spring container close successful");
+        }
     }
 
     public static void main(String... args) throws Exception {
-        EtcdClient client = new EtcdClient();
-
-//        String host = PropertyUtil.getProperty("proxy.host");
-//        Integer port = Integer.valueOf(PropertyUtil.getProperty("proxy.port"));
-
-
-        client.setEndpoints(PropertyUtil.getProperty("registry").split(","));
-        client.setKeepAlive(true);
-        client.init();
-
-        client.watch(PropertyUtil.getProperty("namespace"), event -> {
-            System.out.println(event);
-        });
+        new CraftProxy().proxy();
     }
 }
